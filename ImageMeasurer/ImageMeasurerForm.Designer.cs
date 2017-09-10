dbfa7be89dea1af21807684fff3caf4ca6d9eb9a
Andrew Au <cshung@gmail.com>
2017-09-10 13:13:33 -0700

    Image Measurer

---
+++ ImageMeasurer/ImageMeasurerForm.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace ImageMeasurer
+{
+    partial class ImageMeasurerForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.statusStrip = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.statusStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox.TabIndex = 0;
+            this.pictureBox.TabStop = false;
+            this.pictureBox.Paint += new System.Windows.Forms.PaintEventHandler(this.OnPictureBoxPaint);
+            this.pictureBox.MouseDown += new System.Windows.Forms.MouseEventHandler(this.OnPictureBoxMouseDown);
+            this.pictureBox.MouseMove += new System.Windows.Forms.MouseEventHandler(this.OnPictureBoxMouseMove);
+            this.pictureBox.MouseUp += new System.Windows.Forms.MouseEventHandler(this.OnPictureBoxMouseUp);
+            // 
+            // statusStrip
+            // 
+            this.statusStrip.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.statusStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel});
+            this.statusStrip.Location = new System.Drawing.Point(0, 214);
+            this.statusStrip.Name = "statusStrip";
+            this.statusStrip.Size = new System.Drawing.Size(278, 30);
+            this.statusStrip.TabIndex = 1;
+            // 
+            // toolStripStatusLabel
+            // 
+            this.toolStripStatusLabel.Name = "toolStripStatusLabel";
+            this.toolStripStatusLabel.Size = new System.Drawing.Size(179, 25);
+            this.toolStripStatusLabel.Text = "toolStripStatusLabel1";
+            // 
+            // ImageMeasurerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(278, 244);
+            this.Controls.Add(this.statusStrip);
+            this.Controls.Add(this.pictureBox);
+            this.Name = "ImageMeasurerForm";
+            this.Text = "Image Measurer";
+            this.Load += new System.EventHandler(this.OnImageMeasurerFormLoad);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.statusStrip.ResumeLayout(false);
+            this.statusStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.StatusStrip statusStrip;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel;
+    }
+}
+
---
+++ ImageMeasurer/ImageMeasurerForm.cs
@@ -0,0 +1,98 @@
+﻿namespace ImageMeasurer
+{
+    using System;
+    using System.Drawing;
+    using System.Windows.Forms;
+
+    public partial class ImageMeasurerForm : Form
+    {
+        private Image image;
+        private bool isMouseDown = false;
+        private int sx = 0;
+        private int sy = 0;
+        private int ex = 0;
+        private int ey = 0;
+
+        public ImageMeasurerForm()
+        {
+            this.InitializeComponent();
+        }
+
+        private void OnPictureBoxMouseDown(object sender, MouseEventArgs e)
+        {
+            this.sx = (e.X / 10) * 10;
+            this.sy = (e.Y / 10) * 10;
+            this.ex = this.sx;
+            this.ey = this.sy;
+            this.isMouseDown = true;
+            this.Refresh();
+        }
+
+        private void OnPictureBoxMouseMove(object sender, MouseEventArgs e)
+        {
+            if (this.isMouseDown)
+            {
+                int nx = (e.X / 10) * 10;
+                int ny = (e.Y / 10) * 10;
+                if (this.ex != nx || this.ey != ny)
+                {
+                    this.ex = nx;
+                    this.ey = ny;
+                    this.Refresh();
+                }
+            }
+        }
+
+        private void OnPictureBoxMouseUp(object sender, MouseEventArgs e)
+        {
+            this.isMouseDown = false;
+            this.Refresh();
+        }
+
+        private void OnPictureBoxPaint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            g.DrawImage(this.image, 0, 0);
+            for (int y = 0; y < this.image.Height; y += 10)
+            {
+                g.DrawLine(new Pen(Color.LightGray), 0, y, this.image.Width, y);
+            }
+
+            for (int x = 0; x < this.image.Width; x += 10)
+            {
+                g.DrawLine(new Pen(Color.LightGray), x, 0, x, this.image.Height);
+            }
+
+            if (this.isMouseDown)
+            {
+                int small_x = Math.Min(this.sx, this.ex);
+                int large_x = Math.Max(this.sx, this.ex);
+                int small_y = Math.Min(this.sy, this.ey);
+                int large_y = Math.Max(this.sy, this.ey);
+
+                for (int x = small_x; x < large_x; x++)
+                {
+                    g.FillRectangle(Brushes.Cyan, x, small_y, 10, 10);
+                    g.FillRectangle(Brushes.Cyan, x, large_y, 10, 10);
+                }
+
+                for (int y = small_y; y < large_y; y++)
+                {
+                    g.FillRectangle(Brushes.Cyan, small_x, y, 10, 10);
+                    g.FillRectangle(Brushes.Cyan, large_x, y, 10, 10);
+                }
+
+                this.toolStripStatusLabel.Text = string.Format("({0},{1})-({2},{3})", small_x, small_y, large_x + 10, large_y + 10);
+            }
+        }
+
+        private void OnImageMeasurerFormLoad(object sender, EventArgs e)
+        {
+            this.image = Image.FromFile(@"c:\temp\pic.png");
+            this.pictureBox.Width = this.image.Width;
+            this.pictureBox.Height = this.image.Height;
+            this.ClientSize = new Size(this.pictureBox.Width, this.pictureBox.Height + this.statusStrip.Height);
+            this.Invalidate();
+        }
+    }
+}
