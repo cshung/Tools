7a54c822b58d10364998367cdafd0f76e6ef70e5
Andrew Au <cshung@gmail.com>
2017-10-18 17:12:51 -0700

    DeleteEmptyFolders

---
+++ DeleteEmptyFolders/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace DeleteEmptyFolders
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            string folder = Console.ReadLine();
+            bool deletedSomething = true;
+            while (deletedSomething)
+            {
+                Queue<string> folders = new Queue<string>();
+                if (Directory.Exists(folder))
+                {
+                    folders.Enqueue(folder);
+                }
+
+                deletedSomething = false;
+                while (folders.Count > 0)
+                {
+                    string current = folders.Dequeue();
+
+                    bool hasNoFiles = Directory.EnumerateFiles(current).Count() == 0;
+                    string[] subdirectories = Directory.EnumerateDirectories(current).ToArray();
+                    if (hasNoFiles && subdirectories.Length == 0)
+                    {
+                        Directory.Delete(current);
+                        deletedSomething = true;
+                    }
+                    else
+                    {
+                        foreach (var subdirectory in subdirectories)
+                        {
+                            folders.Enqueue(subdirectory);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
