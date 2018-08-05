80e4fb24e612c4f5f0b0982f4f08c0f65be29896
Andrew Au <cshung@gmail.com>
2018-08-05 13:54:43 -0700

    Alert

---
+++ Alert/MainForm.Designer.cs
@@ -0,0 +1,69 @@
+﻿namespace Alert
+{
+    using System.Drawing;
+    using System.Windows.Forms;
+
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textLabel
+            // 
+            this.textLabel.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.textLabel.AutoSize = true;
+            this.textLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textLabel.Location = new System.Drawing.Point(12, 9);
+            this.textLabel.Name = "textLabel";
+            this.textLabel.Size = new System.Drawing.Size(240, 76);
+            this.textLabel.TabIndex = 0;
+            this.textLabel.Text = "display";
+            this.textLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.textLabel);
+            this.Name = "MainForm";
+            this.Text = "Alert";
+            this.Load += new System.EventHandler(this.OnMainFormLoaded);
+            this.Click += new System.EventHandler(this.OnMainFormClicked);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label textLabel;
+    }
+}
+
