64b6e6f3b062cf5d4c81ad0edb50ee8a69a69389
Andrew Au <cshung@gmail.com>
2020-07-26 10:28:04 -0700

    ImageHighlighter

---
+++ ImageHighlighter/ImageHighlighterForm.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace ImageHighlighter
+{
+    partial class ImageHighlighterForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.menuStrip = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.menuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Location = new System.Drawing.Point(0, 26);
+            this.pictureBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(67, 32);
+            this.pictureBox.TabIndex = 0;
+            this.pictureBox.TabStop = false;
+            this.pictureBox.Paint += new System.Windows.Forms.PaintEventHandler(this.OnPictureBoxPaint);
+            this.pictureBox.MouseDown += new System.Windows.Forms.MouseEventHandler(this.OnPictureBoxMouseDown);
+            this.pictureBox.MouseMove += new System.Windows.Forms.MouseEventHandler(this.OnPictureBoxMouseMove);
+            this.pictureBox.MouseUp += new System.Windows.Forms.MouseEventHandler(this.OnPictureBoxMouseUp);
+            // 
+            // menuStrip
+            // 
+            this.menuStrip.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip.Name = "menuStrip";
+            this.menuStrip.Padding = new System.Windows.Forms.Padding(4, 1, 0, 1);
+            this.menuStrip.Size = new System.Drawing.Size(185, 24);
+            this.menuStrip.TabIndex = 1;
+            this.menuStrip.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 22);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.OnOpenMenuItemClicked);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.OnSaveMenuItemClicked);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.OnExitMenuItemClicked);
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog1";
+            this.openFileDialog.Filter = "Images|*.png";
+            // 
+            // ImageHighlighterForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(185, 159);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.menuStrip);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "ImageHighlighterForm";
+            this.Text = "Image Highlighter";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.menuStrip.ResumeLayout(false);
+            this.menuStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.MenuStrip menuStrip;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog;
+    }
+}
+
---
+++ ImageHighlighter/ImageHighlighterForm.cs
@@ -0,0 +1,121 @@
+﻿namespace ImageHighlighter
+{
+    using System;
+    using System.Drawing;
+    using System.Windows.Forms;
+
+    public partial class ImageHighlighterForm : Form
+    {
+        private const int Resolution = 5;
+        private Bitmap image;
+        private bool isMouseDown = false;
+        private bool isMouseEver = false;
+        private int sx = 0;
+        private int sy = 0;
+        private int ex = 0;
+        private int ey = 0;
+
+        public ImageHighlighterForm()
+        {
+            this.InitializeComponent();
+        }
+
+        private void OnPictureBoxMouseDown(object sender, MouseEventArgs e)
+        {
+            this.sx = (e.X / Resolution) * Resolution;
+            this.sy = (e.Y / Resolution) * Resolution;
+            this.ex = this.sx;
+            this.ey = this.sy;
+            this.isMouseDown = true;
+            this.isMouseEver = true;
+            this.Refresh();
+        }
+
+        private void OnPictureBoxMouseMove(object sender, MouseEventArgs e)
+        {
+            if (this.isMouseDown)
+            {
+                int nx = (e.X / Resolution) * Resolution;
+                int ny = (e.Y / Resolution) * Resolution;
+                if (this.ex != nx || this.ey != ny)
+                {
+                    this.ex = nx;
+                    this.ey = ny;
+                    this.Refresh();
+                }
+            }
+        }
+
+        private void OnPictureBoxMouseUp(object sender, MouseEventArgs e)
+        {
+            this.isMouseDown = false;
+            this.Refresh();
+        }
+
+        private void OnPictureBoxPaint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            if (this.image != null)
+            {
+                g.DrawImage(this.image, 0, 0);
+                if (this.isMouseDown || this.isMouseEver)
+                {
+                    int small_x = Math.Min(this.sx, this.ex);
+                    int large_x = Math.Max(this.sx, this.ex) + Resolution;
+                    int small_y = Math.Min(this.sy, this.ey);
+                    int large_y = Math.Max(this.sy, this.ey) + Resolution;
+                    g.DrawEllipse(new Pen(Color.Cyan, 1), small_x, small_y, large_x - small_x, large_y - small_y);
+                }
+            }
+        }
+
+        private void OnExitMenuItemClicked(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void OnOpenMenuItemClicked(object sender, EventArgs e)
+        {
+            DialogResult dialogResult = this.openFileDialog.ShowDialog();
+            if (dialogResult == DialogResult.OK)
+            {
+                this.image = Image.FromFile(this.openFileDialog.FileName) as Bitmap;
+                this.pictureBox.Width = this.image.Width;
+                this.pictureBox.Height = this.image.Height;
+                this.ClientSize = new Size(this.pictureBox.Width, this.pictureBox.Height + this.menuStrip.Height);
+                this.Invalidate();
+            }
+        }
+
+        private void OnSaveMenuItemClicked(object sender, EventArgs e)
+        {
+            if (this.image != null)
+            {
+                DialogResult dialogResult = this.saveFileDialog.ShowDialog();
+                if (dialogResult == DialogResult.OK)
+                {
+                    Bitmap toSave = new Bitmap(this.image.Width, this.image.Height);
+                    for (int x = 0; x < this.image.Width; x++)
+                    {
+                        for (int y = 0; y < this.image.Height; y++)
+                        {
+                            Color c = this.image.GetPixel(x, y);
+                            double cx = (this.ex + this.sx) / 2.0;
+                            double rx = (this.ex - this.sx) / 2.0;
+                            double cy = (this.ey + this.sy) / 2.0;
+                            double ry = (this.ey - this.sy) / 2.0;
+                            double check = ((x - cx) * (x - cx) / (rx * rx)) + ((y - cy) * (y - cy) / (ry * ry));
+                            double factor = check > 1 ? 0.9 : 1;
+                            int r = (int)(c.R * factor);
+                            int g = (int)(c.G * factor);
+                            int b = (int)(c.B * factor);
+                            toSave.SetPixel(x, y, Color.FromArgb(r, g, b));
+                        }
+                    }
+
+                    toSave.Save(this.saveFileDialog.FileName);
+                }
+            }
+        }
+    }
+}
